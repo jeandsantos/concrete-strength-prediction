a26939e0a9863bd1a18cc9e4f03081c55fb6cc83
Jean Dos Santos <jeandsantos88@gmail.com>
2022-11-09 17:09:21 +0100

    created DataManager and its tests

---
+++ concrete_strength_prediction/utils/data_manager.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from pathlib import Path
+
+class DataManager:
+    """A Class to import and transform data"""
+    
+    def __init__(self, path: str, columns_mapping: dict) -> None:
+        
+        self._path = path
+        self._columns_mapping = columns_mapping
+        
+    def load_dataset(self, *args, **kwargs) -> None:
+        """Load data and save it as an attribute
+
+        Returns:
+            None: None
+        """
+        
+        if Path(self._path).suffix in ['.xls', '.xlsx']:
+            print('Importing files as Excel file')
+            self._df = pd.read_excel(self._path, *args, **kwargs)
+        else:
+            print('Importing files as CSV file')
+            self._df = pd.read_csv(self._path, *args, **kwargs)
+        
+        if self._columns_mapping is not None:
+            self._df = self._df.rename(columns=self._columns_mapping)
+
+    
+    def get_data(self) -> pd.DataFrame:
+    
+        return self._df
+        
+    @property
+    def df(self):
+        return self._df
+
+    @property
+    def path(self):
+        return self._path
+
+    @property
+    def columns_mapping(self):
+        return self._columns_mapping
+
--- tests/utils/test_data_manager.py
+++ tests/utils/test_data_manager.py
@@ -1,10 +1,34 @@
+import pandas as pd
+import numpy as np
+import pytest
+from concrete_strength_prediction.utils.data_manager import DataManager 
 
+path_data = 'https://archive.ics.uci.edu/ml/machine-learning-databases/concrete/compressive/Concrete_Data.xls'
 
+cols_mapping = {
+    'Cement (component 1)(kg in a m^3 mixture)': 'cement',
+    'Blast Furnace Slag (component 2)(kg in a m^3 mixture)': 'slag',
+    'Fly Ash (component 3)(kg in a m^3 mixture)': 'ash',
+    'Water  (component 4)(kg in a m^3 mixture)': 'water',
+    'Superplasticizer (component 5)(kg in a m^3 mixture)': 'superplasticizer',
+    'Coarse Aggregate  (component 6)(kg in a m^3 mixture)': 'coarse_aggregate',
+    'Fine Aggregate (component 7)(kg in a m^3 mixture)': 'fine_aggregate',
+    'Age (day)': 'age',
+    'Concrete compressive strength(MPa, megapascals) ': 'strength',
+}
+
+# TODO create fixture
+dm = DataManager(path_data, cols_mapping)
 
 def test_assert_true():
-    assert True
-    
+    assert True    
 class TestDataManager(object):
+
     
-    def test_imports_file_as_dataframe(self):
-        assert False
+    def test_columns_names_are_based_on_mapping(self):
+        np.array_equal(dm.df.columns.tolist(), list(cols_mapping.values()))
+    dm.load_dataset()
+        
+    def test_imports_as_dataframe(self):
+        assert isinstance(dm.df, pd.DataFrame)
+        assert isinstance(dm.get_data(), pd.DataFrame)
