a6d694b0a952edb97365bb8e798494d4e6d33bc6
Jean Dos Santos <jeandsantos88@gmail.com>
2022-11-15 15:50:14 +0100

    removed mypy + ran black and isort

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,16 @@
 import pytest
 
-from concrete_strength_prediction.utils.data_manager import DataManager
 from concrete_strength_prediction.config.core import config
+from concrete_strength_prediction.utils.data_manager import DataManager
 
-@pytest.fixture(scope='module')
+
+@pytest.fixture(scope="module")
 def dm():
-    
+
     dm = DataManager(
-        path=config.config_app.path_data, 
-        columns_mapping=config.config_model.cols_mapping
-        )
+        path=config.config_app.path_data,
+        columns_mapping=config.config_model.cols_mapping,
+    )
     dm.load_dataset()
 
     yield dm
--- tests/utils/test_data_manager.py
+++ tests/utils/test_data_manager.py
@@ -1,19 +1,21 @@
+import numpy as np
 import pandas as pd
 import pandas.api.types as ptypes
-import numpy as np
 
 from concrete_strength_prediction.config.core import config
 
+
 class TestDataManager(object):
-        
     def test_columns_names_are_based_on_mapping(self, dm):
-        assert np.array_equal(dm.df.columns.tolist(), list(config.config_model.cols_mapping.values()))
-        
+        assert np.array_equal(
+            dm.df.columns.tolist(), list(config.config_model.cols_mapping.values())
+        )
+
     def test_imports_as_dataframe(self, dm):
         assert isinstance(dm.df, pd.DataFrame)
         assert isinstance(dm.get_data(), pd.DataFrame)
- 
+
     def test_data(self, dm):
         assert len(dm.df.shape) == 2
         assert dm.df.shape[1] == len(config.config_model.cols_mapping.keys())
-        assert all([ptypes.is_numeric_dtype(dm.df[col])  for col in dm.df.columns])
+        assert all([ptypes.is_numeric_dtype(dm.df[col]) for col in dm.df.columns])
