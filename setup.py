24440ea13eda45ae47c11e39a5855afcdfac9de9
Jean Dos Santos <jeandsantos88@gmail.com>
2022-11-16 20:24:04 +0100

    fixed issue with missing submodules + fixed links to docs

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 if __name__ == "__main__":
-    setup(version="0.1.3")
+    setup(
+        version="0.1.3",
+        packages=find_packages(),
+    )
